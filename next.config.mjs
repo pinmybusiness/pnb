dee3283e392854c26b8b5d74a727a77dd20d529f
Sanjeev Kumar <sanjeevcse2k23@gmail.com>
2025-09-13 23:07:19 +0530

    csr to ssr

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   reactStrictMode: false, // Disable for MediaPipe to work
+  reactStrictMode: false, // Disable for MediaPipe to work
   images: {
-    domains: ['lh3.googleusercontent.com', 'localhost', 'www.randomstrangerchats.com', 'www.pridelocation.com', 'www.c.animaapp.com'],
+    domains: ['lh3.googleusercontent.com', 'localhost', 'www.pridelocation.com', 'api.fasterq.in'],
   },
+  output: 'standalone', // For AWS Amplify serverful deployment
 };
 
 export default nextConfig;

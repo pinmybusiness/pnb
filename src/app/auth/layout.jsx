edfa893fb9fee4b2af2a433700b63209a0b51c73
Sanjeev Kumar <sanjeevcse2k23@gmail.com>
2025-09-13 23:37:39 +0530

    csr to ssr

--- src/app/auth/layout.jsx
+++
@@ -1,8 +0,0 @@
-// app/auth/layout.jsx
-export default function AuthLayout({ children }) {
-  return (
-    <div className="min-h-screen bg-gray-light flex items-center justify-center p-6 font-sans">
-      <div className="max-w-md w-full">{children}</div>
-    </div>
-  );
-}
